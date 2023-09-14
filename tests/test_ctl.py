eaf42677589ccdb9a761a2280c6d08468c35f4eb
hemant-manwani <hemantmanwani.iitr@gmail.com>
2023-09-14 20:29:00 +0530

    my first commit

---
+++ tests/test_ctl.py
@@ -0,0 +1,13 @@
+from typing import Any
+from unittest.mock import MagicMock, patch
+
+from kubegpt.cli import ctl
+
+
+@patch("kubegpt.cli.subprocess.run")
+def test_get_data_valid(mock_run: Any) -> None:
+    mock_stdout = MagicMock()
+    mock_stdout.configure_mock(stdout="No resources found in default namespace.")
+    mock_run.return_value = mock_stdout
+    result = ctl(["get", "pods"])
+    assert result.stdout == "No resources found in default namespace."
